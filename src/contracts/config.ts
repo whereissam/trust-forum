607580a171c3f48d56171fa45e244ad4920b40a8
Abner <fantasrulai@gmail.com>
2024-11-17 00:03:02 +0700

    update inco network

--- src/contracts/config.ts
+++ src/contracts/config.ts
@@ -1,26 +1,36 @@
 "use client";
 
 import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { createPublicClient, createWalletClient, custom, http } from "viem";
+import { createPublicClient, createWalletClient, custom, defineChain, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
-export const publicClient = createPublicClient({
-  chain: sepolia,
-  transport: http(),
+const incoChain = defineChain({
+  id: 21097,
+  name: "Inco Rivest Testnet",
+  network: "inco_testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "inco",
+    symbol: "inco",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://validator.rivest.inco.org"],
+      webSocket: [],
+    },
+    public: {
+      http: ["https://validator.rivest.inco.org"],
+      webSocket: [],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.rivest.inco.org" },
+  },
+  contracts: {},
 });
 
-export const web3AuthClientId =
-  "BAdZXDm_RIDI4htBGR4qMzMFlA89L5XLXwk5g0rXYTAdnFf1f6XV5Qim7StbPdkTzGFQOQkpe79AfYlboK8ACHM";
-
-export const web3AuthChainConfig = {
-  chainNamespace: CHAIN_NAMESPACES.EIP155,
-  chainId: "0x1", // Please use 0x1 for Ethereum Mainnet
-  rpcTarget: "https://rpc.ankr.com/eth",
-  // Avoid using public rpcTarget in production.
-  // Use services like Infura, Quicknode etc
-  displayName: "Ethereum Mainnet",
-  blockExplorerUrl: "https://etherscan.io/",
-  ticker: "ETH",
-  tickerName: "Ethereum",
-};
+export const publicClient = createPublicClient({
+  chain: incoChain,
+  transport: http(),
+});
